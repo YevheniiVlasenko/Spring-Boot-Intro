a656e97c39148f09e636a207ba12e73f6ab788f0
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-12-12 15:13:39 +0200

    fixed liqubase migration

--- src/main/java/project/bookstore/repository/BookRepository.java
+++ src/main/java/project/bookstore/repository/BookRepository.java
@@ -1,8 +1,10 @@
 package project.bookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.bookstore.model.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
