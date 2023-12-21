4cc660a0d4c6e482834c0f901d0058cd139a113c
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-12-21 13:19:26 +0200

    added create-users-roles-table.yaml, included it to changelog-master

--- src/main/java/project/bookstore/model/Role.java
+++ src/main/java/project/bookstore/model/Role.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 @Entity
 @Data
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
