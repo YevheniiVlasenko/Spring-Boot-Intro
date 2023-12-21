03a0007f50db43be93b62a81c8596b53d1780f34
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-12-21 12:12:37 +0200

    implemented UserDetails interface in the UserClass

--- src/main/java/project/bookstore/model/User.java
+++ src/main/java/project/bookstore/model/User.java
@@ -9,13 +9,17 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Data
 @SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id =?")
 @SQLRestriction("is_deleted = false")
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,4 +39,33 @@ public class User {
 
     private String shippingAddress;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
