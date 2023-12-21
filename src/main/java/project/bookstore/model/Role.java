d87c1206394b21820b59ff45a98098db5c5fc5e6
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-12-21 12:30:56 +0200

    added the Role model with the RoleName enum inside

---
+++ src/main/java/project/bookstore/model/Role.java
@@ -0,0 +1,27 @@
+package project.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Entity
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private String name;
+
+    public enum RoleName {
+        ADMIN,
+        USER
+    }
+}
