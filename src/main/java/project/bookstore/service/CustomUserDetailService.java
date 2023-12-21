814cc4db99775f9152fb19c10cdb0e710ed88f5d
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-12-21 12:22:30 +0200

    added implementation for UserDetailService int the service package

---
+++ src/main/java/project/bookstore/service/CustomUserDetailService.java
@@ -0,0 +1,25 @@
+package project.bookstore.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import project.bookstore.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private static final String USER_BY_EMAIL_EXCEPTION_MSG =
+            "Can't find user with such email";
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException(USER_BY_EMAIL_EXCEPTION_MSG));
+    }
+}
